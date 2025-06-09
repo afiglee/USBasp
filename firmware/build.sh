bc618a480be4ac0329f4a1c55367a70fb52f811b
Dmitriy Fitisov <dmitriy@radier.ca>
2025-06-09 08:42:34 -0400

    Added build script

---
+++ firmware/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if test -d "build"; then
+  echo "Removing old build directory..."
+  rm -rf build
+fi
+mkdir build
+echo "Building the project..."
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain.cmake ..
+make
